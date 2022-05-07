62e0b37712a9c69c6980a293ce3a40bedcd94595
Majician13 <44847744+Majician13@users.noreply.github.com>
2022-05-07 12:08:33 -0400

    added code to show how to use ES6 spread code and examples

--- src/index.js
+++ src/index.js
@@ -3,3 +3,35 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//spread combines both arrays depending on where you put the spread.
+const citrus = ["lime", "lemon", "Orange"];
+const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
+
+const citrus1 = ["lime", "lemon", "Orange"];
+const fruits1 = ["Apple", , "Banana", "Coconut", ...citrus1];
+
+console.log(fruits);
+console.log(fruits1);
+
+const fullName = {
+  fName: "James",
+  lName: "Bond"
+};
+
+//spread "..." places fName/lName into user
+const user = {
+  ...fullName,
+  id: 1,
+  username: "jamesbond007"
+};
+
+//fullName places object and nests fName/lName into fullName into user1.
+const user1 = {
+  fullName,
+  id: 1,
+  username: "jamesbond007"
+};
+
+console.log(user);
+console.log(user1);
